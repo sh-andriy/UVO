3758477207626567a424d82baa20a3846bdfe72b
sh-andriy <sheredko.andriy@gmail.com>
2023-05-28 12:08:52 +0300

    HACKATHON
    - commit

--- proekts/urls.py
+++ proekts/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 app_name = 'proekts'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('add-proekt/', views.add_project, name="add_proekt")
+    path('add-proekt/', views.add_project, name="add_proekt"),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
